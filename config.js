8fc2b6e7d54c87059b890a6bbe6a98c07f29d674
AbhimanyuRajoriya <abhimanyusaini294@gmail.com>
2026-02-25 23:25:54 +0530

    m

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ const CONFIG = {
     COGNITO_REGION: "us-east-1",
     COGNITO_USER_POOL_ID: "us-east-1_RC8qQwJhl",
     COGNITO_CLIENT_ID: "4ks52pngn4nvenaqtup1ogjhod",
-    COGNITO_DOMAIN: "https://us-east-1rc8qqwjhl.auth.us-east-1.amazoncognito.com",
+    COGNITO_DOMAIN: "us-east-1rc8qqwjhl.auth.us-east-1.amazoncognito.com",
     
     // Backend API
     // For local development:
-    API_GATEWAY_ENDPOINT: "ws://54.196.148.112:8000/ws",
-    API_REST_ENDPOINT: "http://54.196.148.112:8000",
+    // API_GATEWAY_ENDPOINT: "ws://54.196.148.112:8000/ws",
+    // API_REST_ENDPOINT: "http://54.196.148.112:8000",
     
     // For production (CloudFront):
-    // API_GATEWAY_ENDPOINT: "wss://54.196.148.112:8000/ws",
-    // API_REST_ENDPOINT: "https://54.196.148.112:8000",
+    API_GATEWAY_ENDPOINT: "wss://d39s9x23h6lb7j.cloudfront.net/ws",
+    API_REST_ENDPOINT: "https://d39s9x23h6lb7j.cloudfront.net",
     
     // WebSocket configuration
     WEBSOCKET_RECONNECT_DELAY: 3000,  // 3 seconds
